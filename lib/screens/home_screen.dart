f783a4392d15adf3088459d7ba26f4514835e579
eretana238 <eretana238@gmail.com>
2020-06-02 23:44:22 -0600

    icon buttons and formatting

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,25 +4,32 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0.0,
-        backgroundColor: Colors.white,
-        actions: <Widget>[
-          Icon(
-            Icons.add,
-            color: Colors.black,
+        appBar: AppBar(
+          elevation: 0.0,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: Icon(
+              Icons.apps,
+              color: Colors.black,
+            ),
+            onPressed: () => print('pressed apps'),
           ),
-        ],
-        leading: Icon(
-          Icons.apps,
-          color: Colors.black,
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 10.0),
+              child: IconButton(
+                  icon: Icon(
+                    Icons.add,
+                    color: Colors.black,
+                    size: 28.0,
+                  ),
+                  onPressed: () => print('pressed add'),
+              ),
+            ),
+          ],
         ),
-      ),
-      body: SafeArea(
-        child: Container(
-
-        ),
-      )
-    );
+        body: SafeArea(
+          child: Container(),
+        ));
   }
 }
